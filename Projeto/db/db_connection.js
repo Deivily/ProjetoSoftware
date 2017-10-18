f0fb7aa29d23040a63c0034a1249620362bb8f7e
Deivily <punksneverdies@hotmail.com>
2017-10-18 16:06:22 -0300

    Refatoração dos módulo item_model e item_controller. Persistência dos itens em banco de dados e listagem dos itens salvos.

--- Projeto/db/db_connection.js
+++ Projeto/db/db_connection.js
@@ -4,6 +4,6 @@ mongoose.connect(dbString, function(err, res) {
     if(err) {
         console.log('Falha na conexão com o banco de dados: ' + dbString + ' !');
     } else {
-        console.log('Conectado ao banco de dados: ' + dbString + ' .');
+        console.log('Conectado ao banco de dados: ' + dbString + '.');
     }
 });
