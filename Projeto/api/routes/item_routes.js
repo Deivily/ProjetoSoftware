a70a0e146edabc4437dba47f59687ab32770e546
Deivily <punksneverdies@hotmail.com>
2017-11-07 16:25:49 -0300

    Refatoração no módulo item_routes: exclusão da variável não utilizada "bodyParser", exclusão das rotas não utilizadas.

--- Projeto/api/routes/item_routes.js
+++ Projeto/api/routes/item_routes.js
@@ -1,16 +1,12 @@
 module.exports = function(app){
 	var itemController = app.controllers.item_controller;
-	var bodyParser = app.bodyParser;
 
-	//app.get('/', cliente.index);
 	app.post('/itens/salvar', itemController.saveItemInDb);
 	app.get('/itens', itemController.getAllInDb);
 	app.get('/itens/id/:idItem', itemController.getByIdInDb);
 	app.get('/itens/nome/:nomeItem', itemController.getByNameInDb);
-	app.get('/itens/parte_nome/:nomeItem', itemController.getByPartOfNameInDb);
 	app.get('/itens/atualizar/:idItem', itemController.updateByIdInDb);
 	app.get('/itens/remover/:idItem', itemController.removeByIdInDb);
-	//app.get('/inicio', itemController.inicial);
 
 	return this;
 };
