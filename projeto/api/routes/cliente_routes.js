a151bb1719dea3aa8c61deafb38dd0a5956ac81f
Deivily <punksneverdies@hotmail.com>
2017-11-13 21:00:43 -0300

    Refatoração no módulo cliente_routes: exclusão da rota que realizava o redirecionamento para o método "getByPartOfNameInDb".

--- projeto/api/routes/cliente_routes.js
+++ projeto/api/routes/cliente_routes.js
@@ -1,15 +1,12 @@
 module.exports = function(app){
 	var clienteController = app.controllers.cliente_controller;
 
-	//app.get('/', cliente.index);
 	app.post('/clientes/salvar', clienteController.saveClienteInDb);
 	app.get('/clientes', clienteController.getAllInDb);
 	app.get('/clientes/id/:idCliente', clienteController.getByIdInDb);
 	app.get('/clientes/nome/:nomeCliente', clienteController.getByNameInDb);
-	app.get('/clientes/parte_nome/:nomeCliente', clienteController.getByPartOfNameInDb);
 	app.get('/clientes/atualizar/:idCliente', clienteController.updateByIdInDb);
 	app.get('/clientes/remover/:idCliente', clienteController.removeByIdInDb);
-	//app.get('/inicio', cliente.inicial);
 
 	return this;
 };
